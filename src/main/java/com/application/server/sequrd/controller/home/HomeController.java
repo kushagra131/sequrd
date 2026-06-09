9fb4b9c228b7ea4695586415eff6add29a14bc85
Kushagra Sharma <kushagra.sharma131@gmail.com>
2026-06-09 14:56:33 +0530

    Java upgrade

--- src/main/java/com/application/server/sequrd/controller/home/HomeController.java
+++ src/main/java/com/application/server/sequrd/controller/home/HomeController.java
@@ -8,30 +8,30 @@ import java.net.URI;
 
 
 /**
- * REST API's Security Best Practices following OWASP secure coding standards.
- * 1. REQUEST's Input Validations / Sanitization:
- *      - Whitelisting prioritizing safe inputs by using an allowlist of trusted inputs.
- *      - Boundary Checks preventing Buffer Overflow attacks.
- *      - Character Escaping mitigating Cross-Site Scripting (XSS) attacks.
- *      - Numeric Range Checks & Data Type Validation ensuring data integrity and accuracy.
- *      - Null Byte / Newline checks preventing exploits.
- *      - Path / Character Encoding checks preventing Path Traversal attacks by using URL encoding.
- * 2. REQUEST's Encoding:
- *      - XSS Encoding prevents Cross-Site Scripting (XSS) attacks by using HTML entity encoding, JavaScript and CSS encoding.
- *      - SQL Injection Encoding prevents SQL Injection attacks by using parameterized queries.
- * 3. Authentication, Authorization (Session Tokens / JWT's, 2FA, SSO, OAuth 2.0) & Password Management (BCrypt, SCrypt & PBKDF2):
- *      - Password Hashing prevents brute force attacks by using a one-way hash function.
- *      - Salt & Iteration Counts preventing dictionary attacks by using a random salt and a high iteration count.
- *      - Storing Passwords in a Secure Storage.
- * 4. Session Management:
- *      - Session Tokens preventing session fixation attacks by using a random token.
- *      - Session Expiration & Invalidation.
- *      - JWT Token Expiration & Invalidation.
- *      - Managing Concurrent Sessions / Logins.
- *      - Concurrent Session Management with Redis & JWT Token Expiration & Invalidation.
- *      - Rate Limiting to prevent DDoS attacks.
- * 5. Logging & Error Handling:
- *      
+ * <p> REST API's Security Best Practices following <b>OWASP Secure</b> Coding Standards. </p>
+ * <p><b> 1. REQUEST's Input Validations / Sanitization: </b></p>
+ * <p> - Whitelisting prioritizing safe inputs by using an allowlist of trusted inputs. </p>
+ * <p> - Boundary Checks preventing Buffer Overflow attacks. </p>
+ * <p> - Character Escaping mitigating Cross-Site Scripting (XSS) attacks. </p>
+ * <p> - Numeric Range Checks & Data Type Validation ensuring data integrity and accuracy. </p>
+ * <p> - Null Byte / Newline checks preventing exploits. </p>
+ * <p> - Path / Character Encoding checks preventing Path Traversal attacks by using URL encoding. </p>
+ * <p><b> 2. REQUEST's Encoding: </b></p>
+ * <p> - XSS Encoding prevents Cross-Site Scripting (XSS) attacks by using HTML entity encoding, JavaScript and CSS encoding. </p>
+ * <p> - SQL Injection Encoding prevents SQL Injection attacks by using parameterized queries. </p>
+ * <p><b> 3. Authentication, Authorization (Session Tokens / JWT's, 2FA, SSO, OAuth 2.0) & Password Management (BCrypt, SCrypt & PBKDF2): </b></p>
+ * <p> - Password Hashing prevents brute force attacks by using a one-way hash function. </p>
+ * <p> - Salt & Iteration Counts preventing dictionary attacks by using a random salt and a high iteration count. </p>
+ * <p> - Storing Passwords in a Secure Storage. </p>
+ * <p><b> 4. Session Management: </b></p>
+ * <p> - Session Tokens preventing session fixation attacks by using a random token. </p>
+ * <p> - Session Expiration & Invalidation. </p>
+ * <p> - JWT Token Expiration & Invalidation. </p>
+ * <p> - Managing Concurrent Sessions / Logins. </p>
+ * <p> - Concurrent Session Management with Redis & JWT Token Expiration & Invalidation. </p>
+ * <p> - Rate Limiting to prevent DDoS attacks. </p>
+ * <p><b> 5. Logging & Error Handling: </b></p>
+ * <p> - Logging all requests and errors to a centralized log file. </p>
  */
 @RestController
 @RequestMapping("/api/v1")
--- src/test/java/com/application/server/sequrd/SequrdApplicationTests.java
+++ src/test/java/com/application/server/sequrd/SequrdApplicationTests.java
@@ -1,11 +1,11 @@
-package com.application.server.sequrd;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SequrdApplicationTests {
-
+//package com.application.server.sequrd;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SequrdApplicationTests {
+//
 //	@Test
 //	void contextLoads() {}
-}
+//}
